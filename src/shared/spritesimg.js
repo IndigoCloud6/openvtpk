8d1d24d4cd1593c7cb8d7c75f331fa33d31a54b4
IndigoYun <maxyun@163.com>
2024-05-30 08:59:39 +0800

    change spritesimg path

--- src/shared/spritesimg.js
+++ src/shared/spritesimg.js
@@ -8,7 +8,7 @@ const path = require('path')
  * @returns {JSON} the content of the root.json file
  */
 const spritesimg = (sourceFolder) => {
-    const rootFileName = path.join(sourceFolder, 'p12/resources/sprites', 'sprite@2x.png')
+    const rootFileName = path.join(sourceFolder, 'p12/resources/sprites', 'sprite.png')
     const rootFile = fs.readFileSync(rootFileName)
     const base64Image = rootFile.toString('base64');
     return base64Image
